965dbfda030afa5114c8a67f32906cef9307eef8
Mohamed Abduljawad <salintofficial@hotmail.com>
2021-03-05 20:19:05 +0200

    Add Firebase Authentication Context by @lambocreeper

---
+++ src/context/FirebaseAuthContext.js
@@ -0,0 +1,59 @@
+import React, { Component, createContext }  from "react";
+import firebaseApp from "../services/FirebaseService";
+
+export const AuthContext = createContext();
+
+export class AuthProvider extends Component {
+    state = {
+        pending: true,
+        user: {}
+    };
+
+    componentDidMount() {
+        firebaseApp.auth().onAuthStateChanged((user) => {
+            this.setState({
+                pending: false,
+                user
+            });
+        });
+    }
+
+    render() {
+        const { children } = this.props;
+        const { pending, user } = this.state;
+
+        if (pending) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <AuthContext.Provider value={{user}}>
+                {children}
+            </AuthContext.Provider>
+        );
+    }
+}
+
+export function IfFirebaseAuthed({ children }) {
+    return (
+        <FirebaseAuthConsumer>
+            {({ user }) => user ? children : <></>}
+        </FirebaseAuthConsumer>
+    );
+}
+
+export function IfFirebaseUnAuthed({ children }) {
+    return (
+        <FirebaseAuthConsumer>
+            {({ user }) => user ? <></> : children}
+        </FirebaseAuthConsumer>
+    );
+}
+
+export function FirebaseAuthConsumer({ children }) {
+    return (
+        <AuthContext.Consumer>
+            {children}
+        </AuthContext.Consumer>
+    );
+}
